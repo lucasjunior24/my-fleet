ea001d703aceb447a70d39c48abe3d432deddb0c
unknown <lucas11souza97@gmail.com>
2024-02-09 23:35:32 -0400

    feat: created project

---
+++ src/screens/SignIn/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components/native";
+import theme from "../../theme";
+
+export const Container = styled.ImageBackground`
+  flex: 1;
+  justify-content: center;
+  padding: 52px;
+  background-color: ${theme.COLORS.GRAY_800};
+`;
