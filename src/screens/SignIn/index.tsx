f47613640fe642477325228fbb280fb779a4183e
unknown <lucas11souza97@gmail.com>
2024-02-18 19:12:38 -0400

    feat: add screen

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,11 +1,12 @@
 import { Container, Slogan, Title } from "./styles";
 import backgroundImg from "../../assets/background.png";
+import { Button } from "../../components/Button";
 export function SignIn() {
   return (
     <Container source={backgroundImg}>
       <Title>Fleet</Title>
-
       <Slogan>Gestão de uso de veículos</Slogan>
+      <Button title="Entrar com Google" />
     </Container>
   );
 }
