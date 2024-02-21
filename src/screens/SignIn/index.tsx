35a4dd508951626bf123f654221e27b3d424aa85
unknown <lucas11souza97@gmail.com>
2024-02-20 22:05:19 -0400

    feat: add env

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,6 +1,7 @@
 import { Container, Slogan, Title } from "./styles";
 import backgroundImg from "../../assets/background.png";
 import { Button } from "../../components/Button";
+
 export function SignIn() {
   return (
     <Container source={backgroundImg}>
